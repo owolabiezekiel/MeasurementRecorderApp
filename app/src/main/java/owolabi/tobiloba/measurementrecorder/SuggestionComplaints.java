c7cd80da65b68e099587985ef9c2aa4604ec30e4
owolabiezekiel <owo.ezekiel@gmail.com>
2019-03-30 10:19:39 +0100

    sending whatsapp messages now works

--- app/src/main/java/owolabi/tobiloba/measurementrecorder/SuggestionComplaints.java
+++ app/src/main/java/owolabi/tobiloba/measurementrecorder/SuggestionComplaints.java
@@ -1,15 +1,21 @@
 package owolabi.tobiloba.measurementrecorder;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.net.URLEncoder;
+
 public class SuggestionComplaints extends AppCompatActivity {
-//textView.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
+    //textView.setJustificationMode(JUSTIFICATION_MODE_INTER_WORD);
     private Button gmailButton;
     private Button twitterButton;
     private Button instagramButton;
@@ -30,28 +36,28 @@ public class SuggestionComplaints extends AppCompatActivity {
         gmailButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                composeEmail( new String[] {"eowolabi537@stu.ui.edu.ng", "owo.ezekiel@gmail.com"}, getString(R.string.complaint_suggestion_mail_topic));
+                composeEmail(new String[]{"eowolabi537@stu.ui.edu.ng", "owo.ezekiel@gmail.com"}, getString(R.string.complaint_suggestion_mail_topic));
             }
         });
 
         twitterButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Twitter button pressed", Toast.LENGTH_LONG).show();
+                sendTwitterDirectMessage(getApplicationContext());
             }
         });
 
         instagramButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Instagram button pressed", Toast.LENGTH_LONG).show();
+                sendInstagramDirectMessage(getApplicationContext());
             }
         });
 
         whatsappButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Whatsapp button pressed", Toast.LENGTH_LONG).show();
+                sendWhatsAppDirectMessage(getApplicationContext());
             }
         });
     }
@@ -65,4 +71,67 @@ public class SuggestionComplaints extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    public void sendTwitterDirectMessage(Context context) {
+        Intent intent = null;
+        try {
+            // get the Twitter app if possible
+            context.getPackageManager().getPackageInfo("com.twitter.android", 0);
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=Toby_Ezekiel"));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        } catch (Exception e) {
+            // no Twitter app, revert to browser
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/Toby_Ezekiel"));
+        }
+        startActivity(intent);
+    }
+
+    public void sendInstagramDirectMessage(Context context) {
+        Intent intent = null;
+        try {
+            // get the Instagram app if possible
+            context.getPackageManager().getPackageInfo("com.instagram.android", 0);
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/_u/tobiloba_oe"));
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        } catch (Exception e) {
+            // no Instagram app, revert to browser
+            intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.instagram.com/_u/tobiloba_oe"));
+        }
+        startActivity(intent);
+    }
+
+    public void sendWhatsAppDirectMessage(Context context){
+        String smsNumber = "23408106723916";
+        boolean isWhatsappInstalled = whatsappInstalledOrNot("com.whatsapp");
+        if (isWhatsappInstalled) {
+
+            Intent sendIntent = new Intent("android.intent.action.MAIN");
+            sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
+            sendIntent.putExtra("jid", PhoneNumberUtils.stripSeparators(smsNumber) + "@s.whatsapp.net");//phone number without "+" prefix
+
+            startActivity(sendIntent);
+        } else {
+            Uri uri = Uri.parse("market://details?id=com.whatsapp");
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            Toast.makeText(this, "WhatsApp not Installed",
+                    Toast.LENGTH_SHORT).show();
+            startActivity(goToMarket);
+        }
+    }
+
+    private boolean whatsappInstalledOrNot(String uri) {
+        PackageManager pm = getPackageManager();
+        boolean app_installed = false;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
+
+
 }
